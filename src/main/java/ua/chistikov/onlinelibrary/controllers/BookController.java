724d70d0994316214b0b92d4a3dd346b19b482f1
vadim.chistikov <vadim1267232@gmail.com>
2022-12-15 22:21:40 +0200

    add the ability to add a book

--- src/main/java/ua/chistikov/onlinelibrary/controllers/BookController.java
+++ src/main/java/ua/chistikov/onlinelibrary/controllers/BookController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ua.chistikov.onlinelibrary.dao.BookDAO;
+import ua.chistikov.onlinelibrary.models.Book;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/book")
@@ -21,5 +24,16 @@ public class BookController {
     public String hello(Model model){
         model.addAttribute("books", bookDAO.index());
         return "book/index";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("book") Book book) {
+        return "book/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") @Valid Book book) {
+        bookDAO.save(book);
+        return "redirect:/book";
     }
 }
